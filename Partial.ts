60d45ce587654dba018aba7b59c579b4d1c02d2e
yosukekato165 <padyk165@gmail.com>
2022-01-15 14:07:17 +0900

    1/15

--- Partial.ts
+++ Partial.ts
@@ -4,6 +4,21 @@ type Parson = {
   gender: "Male" | "Female";
 };
 
+type Body = {
+  weight: number;
+  height?: number;
+};
+
+type Human = Parson & Body;
+
+const Tom: Human = {
+  name: "Tom",
+  age: 18,
+  gender: "Female",
+  weight: 55,
+  height: 165,
+};
+
 // type Partial<T> = {
 //   [P in keyof T]?: T[P];
 // };
