0103cd6e97fb274a08ec60a5edeaf96ae9315bd9
yosukekato165 <padyk165@gmail.com>
2022-01-22 11:39:43 +0900

    add prettier

--- union_intersection.ts
+++ union_intersection.ts
@@ -6,16 +6,16 @@ type d = a & b;
 
 // union　　型はどちらかの型が満たされていればOK
 const union: c = {
-    // a: "tomato",
-    b: 2,
-    k: "red"
+  // a: "tomato",
+  b: 2,
+  k: "red",
 };
 
 // intersection 型は両方の型が満たされていればOK
 const intersection: d = {
-    a: "carrot",
-    b: 2,
-    k: "orange"
+  a: "carrot",
+  b: 2,
+  k: "orange",
 };
 console.log(union);
 console.log(intersection);
