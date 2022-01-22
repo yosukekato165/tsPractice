c7d54b9db5707f8ccbf3162b5150d3ae2dbdc49d
yosukekato165 <padyk165@gmail.com>
2022-01-22 11:06:46 +0900

    touch union_intersection.ts

---
+++ union_intersection.js
@@ -0,0 +1,14 @@
+// union　　型はどちらかの型が満たされていればOK
+var union = {
+    // a: "tomato",
+    b: 2,
+    k: "red"
+};
+// intersection 型は両方の型が満たされていればOK
+var intersection = {
+    a: "carrot",
+    b: 2,
+    k: "orange"
+};
+console.log(union);
+console.log(intersection);
---
+++ union_intersection.ts
@@ -0,0 +1,21 @@
+type a = { a: string };
+type b = { b: number; k: string };
+
+type c = a | b;
+type d = a & b;
+
+// union　　型はどちらかの型が満たされていればOK
+const union: c = {
+    // a: "tomato",
+    b: 2,
+    k: "red"
+};
+
+// intersection 型は両方の型が満たされていればOK
+const intersection: d = {
+    a: "carrot",
+    b: 2,
+    k: "orange"
+};
+console.log(union);
+console.log(intersection);
