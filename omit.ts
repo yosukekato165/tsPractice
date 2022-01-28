b7a51be62c2e5516695ca39ba5784d133ac48b5b
yosukekato165 <padyk165@gmail.com>
2022-01-28 23:11:15 +0900

    omit

--- omit.ts
+++ omit.ts
@@ -1,12 +1,15 @@
 type Todo = {
-    title: string
-    description: string
-    completed: boolean
-}
-type TodoPreview = Omit<Todo, 'description'>
+  title: string;
+  description: string;
+  completed: boolean;
+};
+
+// type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+
+type TodoPreview = Omit<Todo, "description">;
 
 const preview: TodoPreview = {
-    title: '部屋の掃除',
-    completed: false,
-}
-console.log(preview) //=> { title: '部屋の掃除', completed: false }
+  title: "部屋の掃除",
+  completed: false,
+};
+console.log(preview); //=> { title: '部屋の掃除', completed: false }
